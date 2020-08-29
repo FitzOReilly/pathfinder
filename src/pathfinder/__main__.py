8131cc18893140109115d672495c2d073cd6dd1e
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2020-08-29 18:42:44 +0200

    Add Kivy App for Pathfinder

--- src/pathfinder/__main__.py
+++ src/pathfinder/__main__.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 from pathfinder.grid_pathfinder import GridPathfinder
 from pathfinder.grids import WeightedGrid
+from pathfinder.views.kivy_pathfinder_app import KivyPathfinderApp
 from pathfinder.views.tk_grid_view import TkGridView
 
 
@@ -13,6 +14,10 @@ def run():
     window = tk.Tk()
     view.draw_grid(window)
     window.mainloop()
+
+    kivy_app = KivyPathfinderApp()
+    kivy_app.bind(pathfinder)
+    kivy_app.run()
 
 
 if __name__ == "__main__":
