841c66a3ccdbdc62cb55cb5100f30a7a247e9935
FitzOReilly <49785565+FitzOReilly@users.noreply.github.com>
2020-08-29 21:01:51 +0200

    Run tk and Kivy apps simultaneously

--- src/pathfinder/__main__.py
+++ src/pathfinder/__main__.py
@@ -1,3 +1,4 @@
+import threading
 import tkinter as tk
 
 from pathfinder.grid_pathfinder import GridPathfinder
@@ -6,18 +7,31 @@ from pathfinder.views.kivy_pathfinder_app import KivyPathfinderApp
 from pathfinder.views.tk_grid_view import TkGridView
 
 
-def run():
-    grid = WeightedGrid(30, 20)
-    pathfinder = GridPathfinder(grid)
-
+def tk_function(pathfinder):
     view = TkGridView(pathfinder)
     window = tk.Tk()
     view.draw_grid(window)
     window.mainloop()
 
+
+def kivy_function(pathfinder):
     kivy_app = KivyPathfinderApp()
     kivy_app.bind(pathfinder)
     kivy_app.run()
+
+
+def run():
+    grid = WeightedGrid(30, 20)
+    pathfinder = GridPathfinder(grid)
+
+    tk_thread = threading.Thread(target=tk_function, args=(pathfinder,))
+    kivy_thread = threading.Thread(target=kivy_function, args=(pathfinder,))
+
+    tk_thread.start()
+    kivy_thread.start()
+
+    tk_thread.join()
+    kivy_thread.join()
 
 
 if __name__ == "__main__":
